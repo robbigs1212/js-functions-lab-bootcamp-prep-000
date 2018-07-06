7be5686aa74c710897d55c3459e3c745609f270a
ROB BIRMINGHAM <robbigs@gmail.com>
2018-07-06 00:43:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function happyHolidaysTo(Rob) {
 function happyHolidayTo(Independence Day, Rob) {
   return `"Happy"${Independence Day} ${Rob}!`
 }
-
+var mothersday = "Mother's Day"
 function holidayCountdown(days, Mother's Day){
   return `It's ${days} days until ${Mother's Day}
