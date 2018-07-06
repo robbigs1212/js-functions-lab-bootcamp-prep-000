b084bbd7141612cda919d10db66126ee34cc3c39
ROB BIRMINGHAM <robbigs@gmail.com>
2018-07-06 00:58:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var holiday = "Independence Day"
 function happyHolidayTo(holiday,name) {
   return `Happy ${holiday}, ${name}!`
 }
-var holiday = "Mother's Day"
+var holiday = "Mother\'s Day"
 var days = 20
 function holidayCountdown(days, holiday) {
   return `It's ${days} days until ${holiday}`
